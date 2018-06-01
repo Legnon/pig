5615f437c4c1b16569b06f2d58e371cdf5881b3b
Sowon Park <sowonpark@Sowonui-MacBook-Air.local>
2018-06-02 06:22:15 +0900

    <feat> front

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     url(r'^$', views.main, name="main"),
     url(r'^feed/$', views.feed, name="feed"),
     url(r'^meat/$', views.meat, name="meat"),
+    url(r'^feed_1/$', views.feed_1, name="feed_1"),
+    url(r'^feed_2/$', views.feed_2, name="feed_2"),
+    url(r'^feed_3/$', views.feed_3, name="feed_3"),
 ]
--- main/views.py
+++ main/views.py
@@ -6,7 +6,7 @@ def main(request):
     return render(request, "main.html")
 
 
-# 사료 구매하는 페이지
+# 사료 종류 보는 페이지
 def feed(request):
     return render(request, "feed.html")
 
@@ -14,3 +14,19 @@ def feed(request):
 # 고기 구매하는 페이지
 def meat(request):
     return render(request, "meat.html")
+
+
+# 사료 구매(옥수수)하는 페이지
+def feed_1(request):
+    return render(request, "feed_1.html")
+
+
+# 사료 구매(쌀겨)하는 페이지
+def feed_2(request):
+    return render(request, "feed_2.html")
+
+
+# 사료 구매(대두박)하는 페이지
+def feed_3(request):
+    return render(request, "feed_3.html")
+
