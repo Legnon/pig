63c09461993f1c5fee28f08ebdbe2e101ac6392c
KongHyeontaek <rotanev7@gmail.com>
2018-06-02 17:10:49 +0900

    ₩

---
+++ accounts/models.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.db import models
+# Create your models here.
+
+
+class Resume(models.Model):
+    card = models.CharField(max_length=20)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='resume_set')
+
+    def __str__(self):
+        return self.card
