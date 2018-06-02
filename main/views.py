7197f59cc24b938a8e131b70a823b0a298254675
KongHyeontaek <rotanev7@gmail.com>
2018-06-02 21:54:12 +0900

    일반 사용자  구매 못하게

--- main/views.py
+++ main/views.py
@@ -19,48 +19,57 @@ def meat(request):
 
 # 사료 구매(옥수수)하는 페이지
 def feed_1(request):
-    resume = Resume.objects.get(user=request.user)
-    return render(request, "feed_1.html", {
-        "card": resume.card,
-    })
+    if Resume.objects.filter(user=request.user).exists():
+        resume = Resume.objects.get(user=request.user)
+        return render(request, "feed_1.html", {
+            "card": resume.card,
+        })
+    else:
+        return render(request, "feed_1.html")
 
 
 # 사료 구매(쌀겨)하는 페이지
 def feed_2(request):
-    resume = Resume.objects.get(user=request.user)
-    return render(request, "feed_2.html", {
-        "card": resume.card,
-    })
+    if Resume.objects.filter(user=request.user).exists():
+        resume = Resume.objects.get(user=request.user)
+        return render(request, "feed_2.html", {
+            "card": resume.card,
+        })
+    else:
+        return render(request, "feed_2.html")
 
 
 # 사료 구매(대두박)하는 페이지
 def feed_3(request):
-    resume = Resume.objects.get(user=request.user)
-    return render(request, "feed_3.html", {
-        "card": resume.card,
-    })
+    if Resume.objects.filter(user=request.user).exists():
+        resume = Resume.objects.get(user=request.user)
+        return render(request, "feed_3.html", {
+            "card": resume.card,
+        })
+    else:
+        return render(request, "feed_3.html")
 
 
 # 고기 구매(소고기)하는 페이지
 def meat_1(request):
-    resume = Resume.objects.get(user=request.user)
+    # resume = Resume.objects.get(user=request.user)
     return render(request, "meat_1.html", {
-        "location": resume.location,
+        # "location": resume.location,
     })
 
 
 # 고기 구매(돼지고기)하는 페이지
 def meat_2(request):
-    resume = Resume.objects.get(user=request.user)
+    # resume = Resume.objects.get(user=request.user)
     return render(request, "meat_2.html", {
-        "location": resume.location,
+        # "location": resume.location,
     })
 
 
 # 고기 구매(닭고기)하는 페이지
 def meat_3(request):
-    resume = Resume.objects.get(user=request.user)
+    # resume = Resume.objects.get(user=request.user)
     return render(request, "meat_3.html", {
-        "location": resume.location,
+        # "location": resume.location,
     })
 
