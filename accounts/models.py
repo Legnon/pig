2dabf958760f22b9d7f4f264df28f8c933219125
KongHyeontaek <rotanev7@gmail.com>
2018-06-02 19:34:41 +0900

    위치 모델 추가

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Resume(models.Model):
     card = models.CharField(max_length=20)
+    location = models.CharField(max_length=30)
     user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='resume_set')
 
     def __str__(self):
--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from accounts.models import Resume
 
 
 # 사료 구매 / 고기 구매
@@ -18,15 +19,24 @@ def meat(request):
 
 # 사료 구매(옥수수)하는 페이지
 def feed_1(request):
-    return render(request, "feed_1.html")
+    resume = Resume.objects.get(user=request.user)
+    return render(request, "feed_1.html", {
+        "card": resume.card,
+    })
 
 
 # 사료 구매(쌀겨)하는 페이지
 def feed_2(request):
-    return render(request, "feed_2.html")
+    resume = Resume.objects.get(user=request.user)
+    return render(request, "feed_2.html", {
+        "card": resume.card,
+    })
 
 
 # 사료 구매(대두박)하는 페이지
 def feed_3(request):
-    return render(request, "feed_3.html")
+    resume = Resume.objects.get(user=request.user)
+    return render(request, "feed_3.html", {
+        "card": resume.card,
+    })
 
